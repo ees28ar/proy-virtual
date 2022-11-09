fa1a45f0106067922e539dca55ec162c455d76f7
ees28ar <117293997+ees28ar@users.noreply.github.com>
2022-11-09 13:40:10 -0300

    Config MySQL

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'root',
+        'PASSWORD': 'rtm239rtm',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
